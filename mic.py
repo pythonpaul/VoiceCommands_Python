a8e9bdf3b5335d04c493485fe23bf7c8f7593ca2
Paul Sun <paulsun096@gmail.com>
2021-04-09 10:42:19 -0400

    Update mic.py

--- mic.py
+++ mic.py
@@ -1,4 +1,9 @@
 import speech_recognition as sr
 
+'''
+run this code to see available microphone inputs on your computer
+
+'''
+
 for index, name in enumerate(sr.Microphone.list_microphone_names()):
     print("Microphone with name \"{1}\" found for `Microphone(device_index={0})`".format(index, name))
